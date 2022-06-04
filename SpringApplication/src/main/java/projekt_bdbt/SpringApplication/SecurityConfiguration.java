f4187fe1f042743ce69c3b616657ad6794b6f328
atlomak <andrzej.tlomak01@gmail.com>
2022-06-05 01:23:59 +0200

    added few templates and SecurityConfiguration

---
+++ SpringApplication/src/main/java/projekt_bdbt/SpringApplication/SecurityConfiguration.java
@@ -0,0 +1,62 @@
+package projekt_bdbt.SpringApplication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/resources/static/**").permitAll()
+                .antMatchers("/main").authenticated()
+                .antMatchers("/main_admin").access("hasRole('ADMIN')")
+                .antMatchers("/main_user").access("hasRole('USER')")
+                .antMatchers("/main_operator").access("hasRole('OPERATOR')")
+                .antMatchers("/main_klient").access("hasRole('KLIENT')")
+                .antMatchers("/main_konsultant").access("hasRole('KONSULTANT')")
+                .antMatchers("/main_pracownik").access("hasRole('PRACOWNIK')")
+                .antMatchers("/main_sprzedawca").access("hasRole('SPRZEDAWCA')")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/main")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/index")
+                .logoutSuccessUrl("/index")
+                .permitAll();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("user")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN")
+                //poniżej modyfikacja
+                .and()
+                .withUser("operator")
+                .password("operator")
+                .roles("OPERATOR");
+    }
+    @Bean
+    public PasswordEncoder getPasswordEncoder() { return NoOpPasswordEncoder.getInstance();
+    }
+}
