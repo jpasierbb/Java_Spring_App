2b4fd9bcdf75f0c67185a68e8543d6347709e196
atlomak <andrzej.tlomak01@gmail.com>
2022-06-04 22:31:08 +0200

    start

---
+++ SpringApplication/src/test/java/projekt_bdbt/SpringApplication/ApplicationTests.java
@@ -0,0 +1,13 @@
+package projekt_bdbt.SpringApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
