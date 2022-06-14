ecd06925bd02e32983d4e6f99ce02e8a2f229a66
atlomak <andrzej.tlomak01@gmail.com>
2022-06-14 22:45:39 +0200

    Added validation for Client(edit,new)

--- SpringApplication/src/main/java/projekt_bdbt/SpringApplication/CRUD/ClientJoined.java
+++ SpringApplication/src/main/java/projekt_bdbt/SpringApplication/CRUD/ClientJoined.java
@@ -1,15 +1,39 @@
 package projekt_bdbt.SpringApplication.CRUD;
 
+import org.springframework.data.annotation.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 public class ClientJoined {
+    @Id
     int ID_KLIENTA;
+    @Id
     int ID_ADRESU;
+    @NotBlank(message = "Pole nie może być puste")
+    @Size(min = 3,max = 20,message = "Imie musi mieć od 3 do 20 znaków")
     String IMIE;
+    @NotBlank(message = "Pole nie może być puste")
+    @Size(min = 3,max = 20,message = "Nazwisko musi mieć od 3 do 20 znaków")
     String NAZWISKO;
+    @Positive
+    @NotBlank(message = "Pole nie może być puste")
+    @Size(min = 11,max = 11,message = "Pesel musi zawierać 11 cyfr")
     String PESEL;
+    @Positive
     String NUMER_TELEFONU;
+    @NotBlank(message = "Pole nie może być puste")
+    @Size(max = 40, message = "Pole może mieć maksymalnie 40 znaków")
     String MIEJSCOWOSC;
+    @Size(max = 40, message = "Pole może mieć maksymalnie 40 znaków")
+    @NotBlank(message = "Pole nie może być puste")
     String ULICA;
+    @Positive
+    @Size(max = 6,message = "Pole może mieć maksymalnie 6 znaków")
+    @NotBlank(message = "Pole nie może być puste")
     String NUMER_BUDYNKU;
+    @Positive
+    @Size(max = 5, message = "Pole może mieć maksymalnie 5 znaków")
     String NUMER_LOKALU;
 
     public ClientJoined() {
--- SpringApplication/src/main/java/projekt_bdbt/SpringApplication/ClientController.java
+++ SpringApplication/src/main/java/projekt_bdbt/SpringApplication/ClientController.java
@@ -3,9 +3,12 @@ package projekt_bdbt.SpringApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import projekt_bdbt.SpringApplication.CRUD.*;
+
+import javax.validation.Valid;
 
 @Controller
 public class ClientController {
@@ -22,7 +25,10 @@ public class ClientController {
     }
 
     @RequestMapping(value = "/saveClient", method = RequestMethod.POST)
-    public String saveEmployee(@ModelAttribute("client") ClientJoined client) {
+    public String saveEmployee(@Valid @ModelAttribute("client") ClientJoined client, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return "CRUD/new_form_client";
+        }
         clientjoineddao.simpleSave(client);
         return "redirect:/klienci";
     }
@@ -35,7 +41,11 @@ public class ClientController {
         return mav;
     }
     @RequestMapping(value = "/updateClient", method = RequestMethod.POST)
-    public String updateClient(@ModelAttribute("client") ClientJoined client) {
+    public String updateClient(@Valid @ModelAttribute("client") ClientJoined client, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            System.out.println(bindingResult.getAllErrors());
+            return "CRUD/edit_form_client";
+        }
         clientjoineddao.update(client);
         return "redirect:/klienci";
     }
